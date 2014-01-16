c97788ba3fde486557ac1b0790622b7fdf3489fd
Peter Teoh <htmldeveloper@gmail.com>
2014-01-16 11:17:26 +0800

    modified:   flying_plane.py

--- flying_plane.py
+++ flying_plane.py
@@ -6,9 +6,19 @@
 
  http://codeNtronix.com
 """
-import random, sys, math, pygame
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+from math import * # trigonometry
+
+import pygame # just to get a display
+
+import random, sys, math
 from operator import itemgetter
 import time
+
+total_count = 0
 
 class Point3D:
     def __init__(self, x = 0, y = 0, z = 0):
@@ -55,11 +65,14 @@ class Point3D:
         return Point3D(x, y, self.z)
 
 class Simulation:
-    def __init__(self, win_width = 1200, win_height = 800):
+    def __init__(self, win_width = 900, win_height = 800):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((win_width, win_height))
+        self.screen = pygame.display.set_mode((win_width, win_height), pygame.DOUBLEBUF)
         pygame.display.set_caption("Simulation of a rotating plane")
+
+    	glClearColor(0.0, 0.0, 0.0, 1.0)
+    	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         
         self.clock = pygame.time.Clock()
 
@@ -84,23 +97,19 @@ class Simulation:
         self.vertices.append(self.vertices[1].rotateZ(60).rotateX(30))
         self.vertices.append(self.vertices[2].rotateZ(60).rotateX(30))
         self.vertices.append(self.vertices[3].rotateZ(60).rotateX(30))
-        #for v in self.vertices:
-        #        r = v.rotateZ(120)
-	#	t.append(r)
-
-	#self.vertices.append(t)
 
         # Define the vertices that compose each of the 6 faces. These numbers are
         # indices to the vertices list defined above.
         self.faces  = [(0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15)]
 
         # Define colors for each face
-        self.colors = [(255,0,255),(125,125,125),(100,100,100),(111,118,222)]
+        self.colors = [(255,0,255),(0,0,255),(255,0,0),(0,255,255)]
 
         self.angle = 0
         
     def run(self):
         """ Main Loop """
+	global total_count
 
 	PI = 3.141592653
 	theta = 0.0
@@ -108,13 +117,6 @@ class Simulation:
 	theta2 = 0.0
 
 	delta = 0.0
-
-	MAX_X = 1200
-	MAX_Y = 1200
-	MAX_Z = 1200
-	MIN_X = -MAX_X
-	MIN_Y = -MAX_X
-	MIN_Z = -MAX_X
 
         while 1:
             for event in pygame.event.get():
@@ -128,33 +130,20 @@ class Simulation:
             # It will hold transformed vertices.
             t = []
             
-            theta2 += random.randint(0, 3)*theta1
+            theta2 += theta1
 
-            vx=(random.randint(0,3)*(math.sin((theta2-delta)*2))) 
-            vy=(random.randint(0,3)*(math.sin((theta2-delta)*2-PI/2))) 
-            vz=(random.randint(0,3)*(math.sin((theta2-delta)*2-PI/4)))
+	    big_radius = 2
+            vx=big_radius*(math.sin((theta2-delta)*2))
+            vy=big_radius*(math.sin((theta2-delta)*2-PI/2))
+            vz=big_radius*(math.sin((theta2-delta)*2-PI/4))
             for v in self.vertices:
                 # Rotate the point around X axis, then around Y axis, and finally around Z axis.
                 r = v.rotateX(self.angle).rotateY(self.angle).rotateZ(self.angle).translate(Point3D(vx,vy,vz))
                 # Transform the point from 3D to 2D
                 p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
                 # Put the point in the list of transformed vertices
-		if (p.x) > MAX_X:
-			p.x=MAX_X
-		if (p.x) < MIN_X:
-			p.x=MIN_X
-		if (p.y) > MAX_Y:
-			p.y=MAX_Y
-		if (p.y) < MIN_Y:
-			p.y=MIN_Y
-
-		if (p.z) > MAX_Z:
-			p.z=MAX_Z
-		if (p.z) < MIN_Z:
-			p.z=MIN_Z
                 t.append(p)
 
-	    self.vertices = t
             # Calculate the average Z values of each face.
             avg_z = []
             i = 0
@@ -165,6 +154,9 @@ class Simulation:
 
             # Draw the faces using the Painter's algorithm:
             # Distant faces are drawn before the closer ones.
+	    xx=0
+	    yy=0
+	    zz=0
             for tmp in sorted(avg_z,key=itemgetter(1),reverse=True):
                 face_index = tmp[0]
                 f = self.faces[face_index]
@@ -172,19 +164,43 @@ class Simulation:
                              (t[f[1]].x, t[f[1]].y), (t[f[2]].x, t[f[2]].y),
                              (t[f[2]].x, t[f[2]].y), (t[f[3]].x, t[f[3]].y),
                              (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
+		##print t[f[3]].x, t[f[3]].y, t[f[0]].x, t[f[0]].y
         	theta += theta1
-                cx=int(250*(math.sin((theta-delta)*2)+1)+10) % 255
-                cy=int(250*(math.sin((theta-delta)*2-PI/2)+1)+10) % 255
-                cz=int(250*(math.sin((theta-delta)*2-PI/4)+1)+10) % 255
+                cx=int(250*(math.sin((theta-delta)*3)+1)+10) % 255 +1 
+                cy=int(250*(math.sin((theta-delta)*3-PI/2)+1)+10) % 255 +1 
+                cz=int(250*(math.sin((theta-delta)*2-PI/4)+1)+10) % 255 +1 
+		#print "color = %d %d %d"%(cx,cy,cz)
 
-                pygame.draw.polygon(self.screen,(cx,cy,cz),pointlist)
-                ##pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
-
+                #pygame.draw.polygon(self.screen,(cx,cy,cz),pointlist)
+                pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
+		##self.colors[:] = [(x, y/7) for x, y in a]
+		##self.colors[face_index][:] = [((xx + 25 ) % 255, (yy + 25 ) % 255, (zz + 25 ) % 255) for xx,yy,zz in self.colors[face_index]]
+		#xx = (self.colors[face_index][0] + xx + 25) % 255
+		#yy = (self.colors[face_index][1] + yy + 25) % 255
+		#zz = (self.colors[face_index][2] + zz + 25) % 255
+		xx = (xx+self.colors[face_index][0] * 2) % 255 +1 
+		yy = (yy+self.colors[face_index][1] * 4) % 255 +1 
+		zz = (yy+self.colors[face_index][2] * 8) % 255 +1 
+		#print "AAA",self.colors[face_index]
+		#print self.colors[face_index][0]
+		#print self.colors[face_index][1]
+		#print self.colors[face_index][2]
+		self.colors[face_index] = (xx,yy,zz)
+		#print "BBB",self.colors[face_index]
+		#total_count += 1
+		#if total_count > 20:
+                #    	sys.exit()
+		#self.colors[face_index][0] = self.colors[face_index][0] % 255
+		#self.colors[face_index][1] += 25
+		#self.colors[face_index][1] = self.colors[face_index][1] % 255
+		#self.colors[face_index][2] += 25
+		#self.colors[face_index][2] = self.colors[face_index][2] % 255
+#
                 
             self.angle += 1
             
             pygame.display.flip()
-	    time.sleep(0.001)
+#	    time.sleep(0.001)
 
 if __name__ == "__main__":
     Simulation().run()
