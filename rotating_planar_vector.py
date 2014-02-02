db21e122c2c92b97d03bffed3e50ac1e53cb0842
Peter Teoh <htmldeveloper@gmail.com>
2014-02-02 23:53:23 +0800

    modified:   rotating_planar_vector.py

--- rotating_planar_vector.py
+++ rotating_planar_vector.py
@@ -11,264 +11,231 @@ mouseDown = False
 theta = 0.0
 alpha = 0.0
 
-class PlaneCircle3D:
-    def __init__(self, radius = 50.0, ngon = 360):
-        theta = 2 * math.pi / ngon
-	self.vertices=[]
-	for i in range(ngon):
-		x = radius * math.cos(i*theta)
-		z = radius * math.sin(i*theta)
-		y = 0.0
-        	self.vertices.append(Point3D(x,y,z))
+def PlaneCircle3D(radius = 50.0, ngon = 360):
+    theta = 2 * math.pi / ngon
+    vertices=[]
+    for i in range(ngon):
+        x = radius * math.cos(i*theta)
+        z = radius * math.sin(i*theta)
+        y = 0.0
+        vertices.append([x,y,z])
+    return vertices
 
-    def get_vertices(self):
-	return self.vertices
+def rotate_theta_alpha(object, theta, alpha):
+    ##for i in range(len(vertices)):
+    new_vertices=[]
+    for i in range(len(object)):
+	(x,y,z)=object[i]
+        x *= math.sin(theta) * math.cos(alpha)
+        y *= math.cos(theta)
+        z *= math.sin(theta) * math.sin(alpha)
+        #print x,y,z
+        #v.x *= math.sin(theta) * math.cos(alpha)
+        #v.y *= math.cos(theta)
+        #v.z *= math.sin(theta) * math.sin(alpha)
+        new_vertices.append([x,y,z])
+    return new_vertices
 
-    def rotate_theta_alpha(self, theta, alpha):
-	##for i in range(len(vertices)):
-	new_vertices=[]
-        for v in self.vertices:
-		v.x *= math.sin(theta) * math.cos(alpha)
-		v.y *= math.cos(theta)
-		v.z *= math.sin(theta) * math.sin(alpha)
-        	new_vertices.append(Point3D(v.x,v.y,v.z))
-	return new_vertices
-		#self.vertices[i].x *= sin(theta) * cos(alpha)
-		#self.vertices[i].y *= cos(theta)
-		#self.vertices[i].z *= sin(theta) * sin(alpha)
-
-    def rotateX(self, angle):
-        """ Rotates the point around the X axis by the given angle in degrees. """
-	for i in range(len(vertices)):
-		##self.vertices[i].y *= cos(theta)
-		##self.vertices[i].z *= sin(theta) * sin(alpha)
-        	rad = angle * math.pi / 180
-        	cosa = math.cos(rad)
-        	sina = math.sin(rad)
-        	self.vertices[i].y = self.vertices[i].y * cosa - self.vertices[i].z * sina
-        	self.vertices[i].z = self.vertices[i].y * sina + self.vertices[i].z * cosa
- 
-    def rotateY(self, angle):
-        """ Rotates the point around the Y axis by the given angle in degrees. """
+def rotateX(object, angle):
+    """ Rotates the point around the X axis by the given angle in degrees. """
+    for i in range(len(object)):
+        ##vertices[i].y *= cos(theta)
+        ##vertices[i].z *= sin(theta) * sin(alpha)
         rad = angle * math.pi / 180
         cosa = math.cos(rad)
         sina = math.sin(rad)
-        z = self.z * cosa - self.x * sina
-        x = self.z * sina + self.x * cosa
-        return Point3D(x, self.y, z)
- 
-    def rotateZ(self, angle):
-        """ Rotates the point around the Z axis by the given angle in degrees. """
-        rad = angle * math.pi / 180
-        cosa = math.cos(rad)
-        sina = math.sin(rad)
-        x = self.x * cosa - self.y * sina
-        y = self.x * sina + self.y * cosa
-        return Point3D(x, y, self.z)
+        object.vertices[i].y = object.vertices[i].y * cosa - object.vertices[i].z * sina
+        object.vertices[i].z = object.vertices[i].y * sina + object.vertices[i].z * cosa
 
-class Point3D:
-    def __init__(self, x = 0, y = 0, z = 0):
-        self.x, self.y, self.z = float(x), float(y), float(z)
- 
-    def rotateX(self, angle):
-        """ Rotates the point around the X axis by the given angle in degrees. """
-        rad = angle * math.pi / 180
-        cosa = math.cos(rad)
-        sina = math.sin(rad)
-        y = self.y * cosa - self.z * sina
-        z = self.y * sina + self.z * cosa
-        return Point3D(self.x, y, z)
- 
-    def rotateY(self, angle):
-        """ Rotates the point around the Y axis by the given angle in degrees. """
-        rad = angle * math.pi / 180
-        cosa = math.cos(rad)
-        sina = math.sin(rad)
-        z = self.z * cosa - self.x * sina
-        x = self.z * sina + self.x * cosa
-        return Point3D(x, self.y, z)
- 
-    def rotateZ(self, angle):
-        """ Rotates the point around the Z axis by the given angle in degrees. """
-        rad = angle * math.pi / 180
-        cosa = math.cos(rad)
-        sina = math.sin(rad)
-        x = self.x * cosa - self.y * sina
-        y = self.x * sina + self.y * cosa
-        return Point3D(x, y, self.z)
- 
-    def project(self, win_width, win_height, fov, viewer_distance):
-        """ Transforms this 3D point to 2D using a perspective projection. """
-        factor = fov / (viewer_distance + self.z)
-        x = self.x * factor + win_width / 2
-        y = -self.y * factor + win_height / 2
-        return Point3D(x, y, self.z)
+def rotateY(object, angle):
+    """ Rotates the point around the Y axis by the given angle in degrees. """
+    rad = angle * math.pi / 180
+    cosa = math.cos(rad)
+    sina = math.sin(rad)
+    z = object.z * cosa - object.x * sina
+    x = object.z * sina + object.x * cosa
+    return (x, object.y, z)
 
-WIN_WIDTH=640
-WIN_HEIGHT=480
+def rotateZ(object, angle):
+    """ Rotates the point around the Z axis by the given angle in degrees. """
+    rad = angle * math.pi / 180
+    cosa = math.cos(rad)
+    sina = math.sin(rad)
+    x = object.x * cosa - object.y * sina
+    y = object.x * sina + object.y * cosa
+    return (x, y, object.z)
 
-class Simulation:
-    def __init__(self, win_width = WIN_WIDTH, win_height = WIN_HEIGHT):
-        pygame.init()
+win_width=640
+win_height=480
 
-        self.screen = pygame.display.set_mode((win_width, win_height))
-        pygame.display.set_caption("Simulation of a rotating plane")
-        
-        self.clock = pygame.time.Clock()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((win_width, win_height))
 
-        self.vertices = [
-            Point3D(-1,1,-1),
-            Point3D(1,1,-1),
-            Point3D(1,-1,-1),
-            Point3D(-1,-1,-1)
-        ]
+def myinit():
 
-        self.vertices.append(self.vertices[0].rotateX(60))
-        self.vertices.append(self.vertices[1].rotateX(60))
-        self.vertices.append(self.vertices[2].rotateX(60))
-        self.vertices.append(self.vertices[3].rotateX(60))
+    pygame.init()
 
-        self.vertices.append(self.vertices[0].rotateY(120))
-        self.vertices.append(self.vertices[1].rotateY(120))
-        self.vertices.append(self.vertices[2].rotateY(120))
-        self.vertices.append(self.vertices[3].rotateY(120))
-
-        self.vertices.append(self.vertices[0].rotateZ(60).rotateX(30))
-        self.vertices.append(self.vertices[1].rotateZ(60).rotateX(30))
-        self.vertices.append(self.vertices[2].rotateZ(60).rotateX(30))
-        self.vertices.append(self.vertices[3].rotateZ(60).rotateX(30))
-        #for v in self.vertices:
-        #        r = v.rotateZ(120)
-	#	t.append(r)
-
-	#self.vertices.append(t)
-
-        # Define the vertices that compose each of the 6 faces. These numbers are
-        # indices to the vertices list defined above.
-        self.faces  = [(0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15)]
-
-        # Define colors for each face
-        self.colors = [(255,0,255),(125,125,125),(100,100,100),(111,118,222)]
-
-        self.angle = 0
-        
-    def run(self):
-        """ Main Loop """
-        while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            self.clock.tick(50)
-            self.screen.fill((0,32,0))
-
-            # It will hold transformed vertices.
-            t = []
-            
-            for v in self.vertices:
-                # Rotate the point around X axis, then around Y axis, and finally around Z axis.
-                r = v.rotateX(self.angle).rotateY(self.angle).rotateZ(self.angle)
-                # Transform the point from 3D to 2D
-                p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
-                # Put the point in the list of transformed vertices
-                t.append(p)
-
-            # Calculate the average Z values of each face.
-            avg_z = []
-            i = 0
-            for f in self.faces:
-                z = (t[f[0]].z + t[f[1]].z + t[f[2]].z + t[f[3]].z) / 4.0
-                avg_z.append([i,z])
-                i = i + 1
-
-            # Draw the faces using the Painter's algorithm:
-            # Distant faces are drawn before the closer ones.
-            for tmp in sorted(avg_z,key=itemgetter(1),reverse=True):
-                face_index = tmp[0]
-                f = self.faces[face_index]
-                pointlist = [(t[f[0]].x, t[f[0]].y), (t[f[1]].x, t[f[1]].y),
-                             (t[f[1]].x, t[f[1]].y), (t[f[2]].x, t[f[2]].y),
-                             (t[f[2]].x, t[f[2]].y), (t[f[3]].x, t[f[3]].y),
-                             (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
-                pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
-
-                
-            self.angle += 1
-            
-            pygame.display.flip()
+    pygame.display.set_caption("Simulation of a rotating plane")
 
 
-    def myrun(self):
-        """ Main Loop """
-	global mouseDown
-	global theta, alpha
-	LEFT=1
-        while 1:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-	        #elif event.type == KEYDOWN and event.key == K_ESCAPE:
-                #    pygame.quit()
-                #    sys.exit()
-	        #elif event.type == KEYDOWN and event.key == K_p:
-	        #    pygame.image.save(self.screen, "rotating_planar_vector.png")
-	        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
-			x,y = event.pos
-			mouseDown = True
-                	prevx = x 
-                	prevy = y
+    vertices = [
+      [-1,1,-1],
+      [1,1,-1],
+      [1,-1,-1],
+      [-1,-1,-1]
+    ]
 
-		elif event.type == pygame.MOUSEMOTION:
-		        if (mouseDown):
-				x,y=event.pos
-               			theta = x - prevx
-               			alpha = y - prevy
-	        elif event.type == pygame.MOUSEBUTTONUP:
-                	mouseDown = False
+#    vertices.append(rotateX([vertices[0]],60))
+#    vertices.append(rotateX([vertices[1]],60))
+#    vertices.append(rotateX([vertices[2]],60))
+#    vertices.append(rotateX(vertices[3],60))
+#
+#    vertices.append(rotateY(vertices[0],120))
+#    vertices.append(rotateY(vertices[1],120))
+#    vertices.append(rotateY(vertices[2],120))
+#    vertices.append(rotateY(vertices[3],120))
+#
+#    vertices.append(rotateX(rotateZ(vertices[0],60),30))
+#    vertices.append(rotateX(rotateZ(vertices[1],60),30))
+#    vertices.append(rotateX(rotateZ(vertices[2],60),30))
+#    vertices.append(rotateX(rotateZ(vertices[3],60),30))
+    #for v in vertices:
+    #        r = v.rotateZ(120)
+    #       t.append(r)
+
+    #vertices.append(t)
+
+    # Define the vertices that compose each of the 6 faces. These numbers are
+    # indices to the vertices list defined above.
+    faces  = [(0,1,2,3),(4,5,6,7),(8,9,10,11),(12,13,14,15)]
+
+    # Define colors for each face
+    colors = [(255,0,255),(125,125,125),(100,100,100),(111,118,222)]
 
 
+def run():
+    """ Main Loop """
+    while 1:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
 
-#	            body = pm.Body(10, 100)
-#	            body.position = p
-#	            shape = pm.Circle(body, 10, (0,0))
-#	            shape.friction = 0.5
-#	            shape.collision_type = COLLTYPE_BALL
-#	            space.add(body, shape)
-#	            balls.append(shape)
-#	        elif event.type == MOUSEBUTTONDOWN and event.button == 3: 
-#	            if line_point1 is None:
-#	                    line_point1 = Vec2d(event.pos[X], flipy(event.pos[Y]))
-#	        elif event.type == MOUSEBUTTONUP and event.button == 3: 
-#	            if line_point1 is not None:
-#	  
-#	                    line_point2 = Vec2d(event.pos[X], flipy(event.pos[Y]))
-#	                    print line_point1, line_point2
-#	                    body = pm.Body()
-#	                    shape= pm.Segment(body, line_point1, line_point2, 0.0)
-#	                    shape.friction = 0.99
-#	                    space.add(shape)
-#	                    static_lines.append(shape)
-#	                    line_point1 = None
-#	  
-#	            elif event.type == KEYDOWN and event.key == K_SPACE:    
-#	                run_physics = not run_physics
-	  
-            	self.clock.tick(50)
-            	self.screen.fill((0,32,0))
+      clock.tick(10)
+      screen.fill((0,32,0))
 
-	    	vertices=PlaneCircle3D(radius=200.0, ngon=360).rotate_theta_alpha(theta, alpha)
-	    	cx=0
-	    	cy=0
-	    	cz=0
-            	z_angle =  30 * math.pi / 180
-	    	for v in vertices:
-        		new_x = -v.z * math.cos(z_angle) + v.x + WIN_WIDTH/2
-        		new_y = -v.z * math.sin(z_angle) + v.y + WIN_HEIGHT/2
-	    		pygame.draw.line(self.screen,[cz,cx,cy],[WIN_WIDTH/2,WIN_HEIGHT/2],[new_x,new_y],2)
-            
-            	pygame.display.flip()
+      # It will hold transformed vertices.
+      t = []
+
+      angle = 0
+      for v in vertices:
+      # Rotate the point around X axis, then around Y axis, and finally around Z axis.
+	      v = rotateX(v,angle)
+	      v = rotateY(v,angle)
+	      v = rotateZ(v,angle)
+	      # Transform the point from 3D to 2D
+	      p = project(v,screen.get_width(), screen.get_height(), 256, 4)
+	      # Put the point in the list of transformed vertices
+	      t.append(p)
+      
+      # Calculate the average Z values of each face.
+      avg_z = []
+      i = 0
+      for f in faces:
+	      z = (t[f[0]].z + t[f[1]].z + t[f[2]].z + t[f[3]].z) / 4.0
+	      avg_z.append([i,z])
+	      i = i + 1
+      
+      # Draw the faces using the Painter's algorithm:
+      # Distant faces are drawn before the closer ones.
+      for tmp in sorted(avg_z,key=itemgetter(1),reverse=True):
+      	face_index = tmp[0]
+      	f = faces[face_index]
+      	pointlist = [(t[f[0]].x, t[f[0]].y), (t[f[1]].x, t[f[1]].y), (t[f[1]].x, t[f[1]].y), (t[f[2]].x, t[f[2]].y), (t[f[2]].x, t[f[2]].y), (t[f[3]].x, t[f[3]].y), (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)]
+        print t[f[3]].x, t[f[3]].y, t[f[0]].x, t[f[0]].y
+        pygame.draw.polygon(screen,colors[face_index],pointlist)
+      
+      
+      angle += 1
+      
+      pygame.display.flip()
+
+def myrun():
+    """ Main Loop """
+    global mouseDown
+    global theta, alpha
+    global clock
+    global screen
+
+    LEFT=1
+    myinit()
+    while 1:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+        #elif event.type == KEYDOWN and event.key == K_ESCAPE:
+        #    pygame.quit()
+        #    sys.exit()
+        #elif event.type == KEYDOWN and event.key == K_p:
+        #    pygame.image.save(screen, "rotating_planar_vector.png")
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
+            x,y = event.pos
+            mouseDown = True
+            prevx = x
+            prevy = y
+
+        elif event.type == pygame.MOUSEMOTION:
+            if (mouseDown):
+                x,y=event.pos
+                theta = x - prevx
+                alpha = y - prevy
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouseDown = False
+
+#                   body = pm.Body(10, 100)
+#                   body.position = p
+#                   shape = pm.Circle(body, 10, (0,0))
+#                   shape.friction = 0.5
+#                   shape.collision_type = COLLTYPE_BALL
+#                   space.add(body, shape)
+#                   balls.append(shape)
+#               elif event.type == MOUSEBUTTONDOWN and event.button == 3:
+#                   if line_point1 is None:
+#                           line_point1 = Vec2d(event.pos[X], flipy(event.pos[Y]))
+#               elif event.type == MOUSEBUTTONUP and event.button == 3:
+#                   if line_point1 is not None:
+#
+#                           line_point2 = Vec2d(event.pos[X], flipy(event.pos[Y]))
+#                           print line_point1, line_point2
+#                           body = pm.Body()
+#                           shape= pm.Segment(body, line_point1, line_point2, 0.0)
+#                           shape.friction = 0.99
+#                           space.add(shape)
+#                           static_lines.append(shape)
+#                           line_point1 = None
+#
+#                   elif event.type == KEYDOWN and event.key == K_SPACE:
+#                       run_physics = not run_physics
+
+        clock.tick(50)
+        screen.fill((0,32,0))
+
+        vertices=rotate_theta_alpha(PlaneCircle3D(radius=200.0, ngon=360), theta, alpha)
+	theta += 0.005
+	alpha += 0.005
+        cx=0
+        cy=0
+        cz=0
+        z_angle =  30 * math.pi / 180
+        for i in range(len(vertices)):
+	    (x,y,z)=vertices[i]
+            new_x = -z * math.cos(z_angle) + x + win_width/2
+            new_y = -z * math.sin(z_angle) + y + win_height/2
+            ##print vertices[i].x, vertices[i].y, vertices[i].z, new_x, new_y
+            pygame.draw.line(screen,[cz,cx,cy],[win_width/2,win_height/2],[new_x,new_y],2)
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
-    Simulation().myrun()
+  myrun()
