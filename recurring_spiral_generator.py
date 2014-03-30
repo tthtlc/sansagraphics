94e832247ec33b44a8bceae633b1ed1e89da9d00
Peter Teoh <htmldeveloper@gmail.com>
2014-03-30 15:12:43 +0800

    new file:   recurring_spiral_generator.py modified:   spiral_generator.py

--- recurring_spiral_generator.py
+++ recurring_spiral_generator.py
@@ -28,6 +28,7 @@ centerx=xscreen/2.0
 centery=yscreen/2.0
 size = [xscreen,yscreen]
 screen = pygame.display.set_mode(size)
+fraction=4.0/5.0
  
 pygame.display.set_caption("Sansagraphics World")
  
@@ -39,13 +40,20 @@ xorigin=999.9
 yorigin=999.9
 n=3
 max_spiral_angle=PI/3.0
+global_angle=PI/n
 
 def spiral(n, min_radius, max_radius, nos_parts, centerx, centery, max_spiral_angle):
+    global fraction
+    global global_angle
     gap = (max_radius - min_radius)/nos_parts
     angle_increment=max_spiral_angle/nos_parts
     phi = 2*PI/n
     xorigin=999.9
 
+    cx=int(120*(math.sin(2*(global_angle))+1)+10)
+    cy=int(120*(math.sin(global_angle)+1)+10)
+    cz=int(120*(math.sin(global_angle-PI)+1)+10)
+    global_angle = global_angle + PI/n
     for i in range(0, n+1):
 	angle = i*phi
 	radius = min_radius
@@ -58,13 +66,15 @@ def spiral(n, min_radius, max_radius, nos_parts, centerx, centery, max_spiral_an
 		spiral_angle=spiral_angle + angle_increment
 
 		if (j>0) and (xorigin!=999.9):
-			cx=int(120*(math.sin(2*(phi))+1)+10)
-			cy=int(120*(math.sin(phi)+1)+10)
-			cz=int(120*(math.sin(phi-PI)+1)+10)
        		 	pygame.draw.line(screen,[cx,cy,cz],[xorigin,yorigin],[x_offset,y_offset],2)
 
 		xorigin = x_offset
 		yorigin = y_offset
+	if (min_radius*fraction> 6.0):
+		spiral(n, min_radius*fraction, max_radius*fraction, nos_parts, xorigin, yorigin, max_spiral_angle)
+
+print "Hey you....trying min_radius*fraction>7.0"
+print "don't be lazy....change 7.0 to other values and see!!! "
 
 while done == False:
  
