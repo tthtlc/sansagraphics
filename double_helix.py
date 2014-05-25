796816c8e5483e46030b807f8b671117e9cfe979
Peter Teoh <htmldeveloper@gmail.com>
2014-05-25 22:36:19 +0800

    modified:   double_helix.py

--- double_helix.py
+++ double_helix.py
@@ -21,11 +21,11 @@ pygame.display.set_mode((800,600), pygame.OPENGL|pygame.DOUBLEBUF)
 
 done = False
 
-t=600
+t=0
 
 while not done:
 
-    t=t+3
+    t=t+1
     
     # for fun comment out these two lines
 
@@ -38,8 +38,6 @@ while not done:
 
     glLoadIdentity()
     gluPerspective(90,1,0.01,1000)
-
-    ## this will rotate the angle of view all the time, approximately around the x-z axis
     gluLookAt(sin(t/200.0)*3,sin(t/500.0)*3,cos(t/200.0)*3,0,0,0,0,1,0)
 
     # Draw the helix (this ought to be a display list call)
@@ -88,7 +86,6 @@ while not done:
     glTexImage2Df(GL_TEXTURE_2D, 0,4,0,GL_RGBA,
                   texdata)
 
-
     glEnable(GL_BLEND);
     glBlendFunc (GL_SRC_ALPHA, GL_ONE); # XXX Why GL_ONE?
 # alternatively:
@@ -103,33 +100,24 @@ while not done:
     glBegin(GL_TRIANGLE_STRIP);
     #pulse2 = 0.5
 
-    r=1.1 # try other values - integers as well
-    R=1.5 # try other values - integers as well
-    rr=3.0 # try other values - integers as well
-    kw=6*r
-    jw=4*r
-    iw=5*r
-    d=1   # try other values
-	
-    ## rotating around the y axis , so only x and z coord depends on k
-    for k in range(0,10):
-    ## x-z plane circle rotating around the y axis , so only x and z plane will generate a circle (generator is j) and projected on y (generator is i)
-    	for i in range(0,25):
-    		for j in range(0,25):
+    for i in range(0,100):
 
-			#pulse2 += 0.5
-		        if (i%2==0):
-		            glTexCoord2f(0,i);
-		            #glVertex3f( R*cos(i/r)+r*sin(j/r), R*cos(j/r), R*sin(j/r)+r*sin(i/r));            
-		            ##glVertex3f( rr*cos(k/rr)+R*cos(i/r/4)*sin(j/r), R*cos(j/r), rr*sin(k/rr)+R*sin(j/r)*sin(i/r/4));            
-		            glVertex3f( rr*cos(k/kw)+R*cos(i/iw)*sin(j/jw), R*cos(j/jw), rr*sin(k/kw)+R*sin(j/jw)*sin(i/iw));            
-		           ## glVertex3f( cos(i/r)*sin(j/r), -2.5+i*0.05, sin(i/r));            
-		#            glVertex3f( cos(i/r)*pulse2, -2.5+i*0.05, sin(i/r)*pulse2);
-		        else:
-		            glTexCoord2f(i%10,i);
-		            glVertex3f( rr*cos(k/kw)+R*cos(i/iw+3.14)*sin(j/jw), R*cos(j/jw+3.14), rr*sin(k/kw)+R*sin(j/jw)*sin(i/iw+3.14));            
-		            #glVertex3f( R*cos(i/r+3.14)+r*sin(j/r), R*cos(j/r+3.14), R*sin(j/r)+r*sin(i/r+3.14));            
-		#            glVertex3f( cos(i/r+3.14)*pulse2, -2.5+i*0.05+d+pulse2*1, sin(i/r+3.14)*pulse2);
+        r=5.0 # try other values - integers as well
+        d=1   # try other values
+
+	#pulse2 += 0.5
+        #if (i%2==0):
+        #    glTexCoord2f(0,i);
+        #    glVertex3f( cos(i/r), -2.5+i*0.05, sin(i/r));            
+#            glVertex3f( cos(i/r)*pulse2, -2.5+i*0.05, sin(i/r)*pulse2);
+        #else:
+        #glTexCoord2f(1,i);
+######### to do:   
+	change the y axiss via a loop round the z axis.
+        glVertex3f( cos(i/r+3.14), -2.5+i*0.05+d, sin(i/r+3.14));
+        #glVertex3f( cos(i/r)*pulse2, -2.5+i*0.05, sin(i/r)*pulse2);
+        glVertex3f( cos(i/r), -2.5+i*0.05, sin(i/r));            
+#            glVertex3f( cos(i/r+3.14)*pulse2, -2.5+i*0.05+d+pulse2*1, sin(i/r+3.14)*pulse2);
         
 
     glEnd();
