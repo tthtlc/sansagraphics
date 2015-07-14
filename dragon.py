f082949484f656dc2cd27fc0672e27c8c7a5a3d9
Peter Teoh <htmldeveloper@gmail.com>
2015-07-14 08:13:51 +0800

    modified:   dragon.py

--- dragon.py
+++ dragon.py
@@ -8,23 +8,28 @@ p.speed(100)
 pencolour=["green","cyan","blue","violet", "red", "yellow"]
 t.pencolor(pencolour[2])
 
-def dragon(t, length, level):
+def dragonY(length, level):
+	global t
 	if level>0:
-		t.left(90)
+		t.right(45)
 		t.forward(length/2)
-		dragon(t, length/2, level-1)
+		dragonX(length/2,level-1)
+		t.right(45)
+		dragonY(length/2,level-1)
+	#else:
+	#	t.forward(length/2)
+def dragonX(length, level):
+	global t
+	if level>0:
+		dragonX(length/2,level-1)
+		t.left(45)
+		dragonY(length/2,level-1)
 		t.forward(length/2)
-		t.right(90)
-		t.forward(length/2)
-		dragon(t, length/2, level-1)
-		t.forward(length/2)
-		t.right(90)
-		t.forward(length/2)
-		dragon(t, length/2, level-1)
-		t.forward(length/2)
-		t.left(90)
+		t.left(45)
+	#else:
+	#	t.forward(length/2)
 	
-dragon(t, 100, 5)
+dragonX(100, 5)
 raw_input()
 sys.exit(0)
 
