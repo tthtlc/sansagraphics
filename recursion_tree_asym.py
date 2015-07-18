5b8db212e64b51083263693ce5e7c14825934fea
Peter Teoh <htmldeveloper@gmail.com>
2015-07-18 20:56:55 +0800

    new file:   recursion_tree.py new file:   recursion_tree_asym.py

---
+++ recursion_tree_asym.py
@@ -0,0 +1,29 @@
+import sys
+import turtle as t
+
+p = t.Pen()
+p.reset()
+p.down()
+p.speed(100)
+pencolour=["green","cyan","blue","violet", "red", "yellow"]
+t.pencolor(pencolour[2])
+
+def asym_tree(t, length, level):
+	if level>0:
+		t.forward(length*2/3)
+		t.right(30)
+		t.forward(length*3/4)
+		asym_tree(t, length/2, level-1)
+		t.forward(-length*3/4)
+		t.left(60)
+		t.forward(length/2)
+		asym_tree(t, length/2, level-1)
+		t.forward(-length/2)
+		t.right(30)
+		t.forward(-length*2/3)
+	
+asym_tree(t, 200, 5)
+raw_input()
+sys.exit(0)
+
+
