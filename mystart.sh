cd42d61f375dd4993f8bad17df311c77ac7ccdc2
Peter Teoh <htmldeveloper@gmail.com>
2018-08-09 00:08:36 +0800

    Changes to be committed: new file:   mystart.sh

---
+++ mystart.sh
@@ -0,0 +1,8 @@
+python multi_sinusoidal_solid.py 200
+python multi_sinusoidal_solidv2.py 200
+python multi_sinusoidal_solidv3.py 200
+python multi_sinusoidal_solidv4.py 200
+python multi_sinusoidal_solidv5.py 200
+python multi-tetrahedron-multicolor.py 200
+python multi-tetrahedron.py 200
+python rotating_multiprong.py 200
